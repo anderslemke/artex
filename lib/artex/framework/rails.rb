27d5fe6e3e48fb4ea65711640769c731cb33976a
Niklas Hofer <niklas+dev@lanpartei.de>
2013-10-23 18:14:12 +0200

    decide between #latex_escape and #localize in #l helper

--- lib/artex/framework/rails.rb
+++ lib/artex/framework/rails.rb
@@ -12,16 +12,19 @@ module ArTeX
       module HelperMethods
         # Similar to h()
         def latex_escape(*args)
+          ArTeX::Document.escape(*args)
+        end
+
+        def l(*args)
           # Since Rails' I18n implementation aliases l() to localize(), LaTeX
           # escaping should only be done if ArTeX is doing the rendering.
           # Otherwise, control should be be passed to localize().
           if Thread.current[:_rendering_rtex]
-            ArTeX::Document.escape(*args)
+            latex_escape(*args)
           else
             localize(*args)
           end
         end
-        alias :l :latex_escape
       end
 
       # action-specific options can be passed from the view by setting
