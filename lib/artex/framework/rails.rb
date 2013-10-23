d8087b612a44d1d4c1b547f5d33ca637d389e88f
Niklas Hofer <niklas+dev@lanpartei.de>
2013-10-23 18:06:40 +0200

    implement template handler, rails 3 style

--- lib/artex/framework/rails.rb
+++ lib/artex/framework/rails.rb
@@ -6,6 +6,7 @@ module ArTeX
       
       def self.setup
         ArTeX::Document.options[:tempdir] = File.expand_path(File.join(RAILS_ROOT, 'tmp'))
+        ActionView::Template.register_template_handler(:rtex, TemplateHandler)
       end
             
       module HelperMethods
@@ -21,6 +22,31 @@ module ArTeX
           end
         end
         alias :l :latex_escape
+      end
+
+      # action-specific options can be passed from the view by setting
+      # @latex_options, for example:
+      #
+      #    @latex_options = { preprocess: true }
+      #
+      # WARNING: MUST use a layout, else only pure latex will be returned by response
+      class TemplateHandler < ::ActionView::Template::Handlers::ERB
+        def self.call(template)
+          new.compile(template)
+        end
+
+        def compile(template)
+          erb = "<% __in_erb_template=true %>#{template.source}"
+          return <<-EORUBY
+            source = #{self.class.erb_implementation.new(erb, :trim=>(self.class.erb_trim_mode == "-")).src}
+            if content_for?(:layout) # (inner) already rendered
+              ::ArTeX::Document.new(content_for(:layout), (@latex_options || {}).merge(layout: source)).to_pdf(binding)
+            else
+              # in the view, just parse erb and wait for layout
+              source
+            end
+          EORUBY
+        end
       end
       
     end
